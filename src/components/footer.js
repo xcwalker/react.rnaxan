1bd8506a62d8418a2d44ae678d48f4845eb87603
XCWalker | Xander Walker <xcwalker21@gmail.com>
2022-12-17 15:37:51 +0000

    Rx: Components

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+export function Footer() {
+    return <footer className="footer" id="footer">
+        <div className="container">
+            <h1>Rnaxan Footer</h1>
+        </div>
+    </footer>
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,7 @@
+export function Navbar() {
+    return <header className="navbar" id="navbar">
+        <div className="container">
+            <h1>Rnaxan Footer</h1>
+        </div>
+    </header>
+}
